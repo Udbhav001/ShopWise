2fb7c6f3f8ba1c9c87054d97d6d83aaa065bdc73
Udbhav <udbhava001@gmail.com>
2022-03-21 02:44:22 +0530

    Created Project Structure

---
+++ ShopWiseProject/ShopWiseWebParent/ShopWiseBackEnd/src/main/java/com/shopwise/admin/ShopWiseBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopwise.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopWiseBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopWiseBackEndApplication.class, args);
+	}
+
+}
